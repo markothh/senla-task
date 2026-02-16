bf0f7dbe98a69489cce2d0500ffc49ffffeca717
Markothh <markothh@>
2026-02-16 15:34:51 +0300

    task-15.2

---
+++ task-12/core/src/main/resources/db/migration/V6__insert_users.sql
@@ -0,0 +1,9 @@
+insert into users (name, password, role) values
+    ('markothh', 'admin', 'ADMIN'),
+    ('user1', '123123', 'USER'),
+    ('user2', '123123', 'USER'),
+    ('ivan', 'admin', 'ADMIN'),
+    ('user3', '123123', 'USER'),
+    ('admin', 'admin', 'ADMIN'),
+    ('user4', '456456', 'USER'),
+    ('usernull', '456456', 'USER');
